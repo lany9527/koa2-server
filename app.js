c6c661c7ef1db054298d3727cbd00f7d0b9591d4
lany9527 <lany9527@gmail.com>
2017-04-25 14:49:30 +0800

    用koa2脚手架重新生成项目

--- app.js
+++ app.js
@@ -1,8 +1,70 @@
 const Koa = require('koa');
-const app = new Koa;
+const app = new Koa();
 const router = require('koa-router')();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser')();
+const logger = require('koa-logger');
 
-const router = require('./routes/routes')
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-app.use(routes())
+//log工具
+const logUtil = require('./utils/log_util');
+// api
+const api = require('./routes/api');
+//格式化输出
+const response_formatter = require('./middlewares/response_formatter');
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(bodyparser);
+app.use(json());
+app.use(logger());
+app.use(require('koa-static')(__dirname + '/public'));
+
+app.use(views(__dirname + '/views', {
+  extension: 'jade'
+}));
+
+// logger
+app.use(async (ctx, next) => {
+  //响应开始时间
+  const start = new Date();
+  //响应间隔时间
+  var ms;
+  try {
+    //开始进入到下一个中间件
+    await next();
+
+    ms = new Date() - start;
+    //记录响应日志
+    logUtil.logResponse(ctx, ms);
+
+  } catch (error) {
+
+    ms = new Date() - start;
+    //记录异常日志
+    logUtil.logError(ctx, error, ms);
+  }
+});
+
+// user api
+//添加格式化处理响应结果的中间件，在添加路由之前调用
+//仅对/api开头的url进行格式化处理
+app.use(response_formatter('^/api'));
+
+router.use('/', index.routes(), index.allowedMethods());
+router.use('/users', users.routes(), users.allowedMethods());
+router.use('/api', api.routes(), api.allowedMethods());
+// 不调用此句将无法得到结果
+app.use(router.routes(), router.allowedMethods());
+// routes
+// app.use(index.routes(), index.allowedMethods());
+// app.use(users.routes(), users.allowedMethods());
+
+
+module.exports = app;
